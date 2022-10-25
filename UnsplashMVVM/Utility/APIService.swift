e6e0c72cd82e5e56b6ff3b7ebf706e94cccb941e
Glsme <glasses.str.man@gmail.com>
2022-10-25 22:47:36 +0900

    Feat: APISercvice 추가

--- UnsplashMVVM/Utility/APIService.swift
+++ UnsplashMVVM/Utility/APIService.swift
@@ -6,7 +6,22 @@
 //
 
 import Foundation
+import Alamofire
 
 class APIService {
-    static func searchPhoto(query: String, completion: @escaping ())
+    static func searchPhoto(query: String, completion: @escaping (SearchPhoto?, Int?, Error?) -> Void) {
+        let url = "\(APIKey.searchURL)\(query)"
+        let header: HTTPHeaders = ["Authorization": APIKey.authorization]
+        
+        AF.request(url, method: .get, headers: header).responseDecodable(of: SearchPhoto.self) { respose in
+            let statusCode = respose.response?.statusCode
+            
+            switch respose.result {
+            case .success(let value): completion(value, statusCode, nil)
+            case .failure(let error): completion(nil, statusCode, error)
+            }
+        }
+    }
+    
+    private init() { }
 }
