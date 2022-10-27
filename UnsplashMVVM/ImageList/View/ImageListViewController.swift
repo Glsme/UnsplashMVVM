13cbc9d52f14cd747055d1ae739b83da0d467ab4
Glsme <glasses.str.man@gmail.com>
2022-10-27 19:00:19 +0900

    Refactor: Diffable Collection View 코드 개선 작업

--- UnsplashMVVM/ImageList/View/ImageListViewController.swift
+++ UnsplashMVVM/ImageList/View/ImageListViewController.swift
@@ -1,60 +1,25 @@
 //
-//  ViewController.swift
+//  ImageListViewController.swift
 //  UnsplashMVVM
 //
-//  Created by Seokjune Hong on 2022/10/25.
+//  Created by Seokjune Hong on 2022/10/27.
 //
 
 import UIKit
-import RxCocoa
-import RxSwift
 
 class ImageListViewController: UIViewController {
     
-    @IBOutlet weak var searchbar: UISearchBar!
+    @IBOutlet weak var ImageSearchbar: UISearchBar!
     @IBOutlet weak var imageCollectionView: UICollectionView!
     
-    private var dataSource: UICollectionViewDiffableDataSource<Int, SearchResult>!
-    
     let viewModel = ImageListViewModel()
-    let disposeBag = DisposeBag()
+    private var dataSource: UICollectionViewDiffableDataSource<Int, String>!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        configureUI()
-        bindData()
-    }
-    
-    func configureUI() {
         imageCollectionView.collectionViewLayout = createLayout()
         configureDataSource()
-        imageCollectionView.delegate = self
-        searchbar.delegate = self
-    }
-    
-    func bindData() {
-        viewModel.photoList
-            .withUnretained(self)
-            .bind { (vc, photo) in
-                var snapshot = NSDiffableDataSourceSnapshot<Int, SearchResult>()
-                snapshot.appendSections([0])
-                snapshot.appendItems(photo.results)
-                vc.dataSource.apply(snapshot, animatingDifferences: false)
-            }
-            .disposed(by: disposeBag)
-    }
-}
-
-extension ImageListViewController: UISearchBarDelegate {
-    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
-        viewModel.requestSearchPhoto(query: searchbar.text!)
-    }
-}
-
-extension ImageListViewController: UICollectionViewDelegate {
-    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        print(indexPath)
     }
 }
 
@@ -62,30 +27,24 @@ extension ImageListViewController {
     private func createLayout() -> UICollectionViewLayout {
         let config = UICollectionLayoutListConfiguration(appearance: .insetGrouped)
         let layout = UICollectionViewCompositionalLayout.list(using: config)
-        
         return layout
     }
     
     private func configureDataSource() {
-        let cellRegistration = UICollectionView.CellRegistration<UICollectionViewListCell, SearchResult> { cell, indexPath, itemIdentifier in
+        let cellRegistration = UICollectionView.CellRegistration<UICollectionViewListCell, String> { cell, indexPath, itemIdentifier in
             var content = UIListContentConfiguration.valueCell()
-            content.text = "\(itemIdentifier.likes)"
-            
-            DispatchQueue.global().async {
-                let url = URL(string: itemIdentifier.urls.thumb)!
-                let data = try? Data(contentsOf: url)
-                
-                DispatchQueue.main.async {
-                    content.image = UIImage(data: data!)
-                    cell.contentConfiguration = content
-                }
-            }
+            content.text = itemIdentifier
+            cell.contentConfiguration = content
         }
         
         dataSource = UICollectionViewDiffableDataSource(collectionView: imageCollectionView, cellProvider: { collectionView, indexPath, itemIdentifier in
             let cell = collectionView.dequeueConfiguredReusableCell(using: cellRegistration, for: indexPath, item: itemIdentifier)
             return cell
         })
+        
+        var snapshot = NSDiffableDataSourceSnapshot<Int, String>()
+        snapshot.appendSections([0])
+        snapshot.appendItems("어우 개배불러".map { String($0) })
+        dataSource.apply(snapshot)
     }
 }
-
