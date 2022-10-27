ee60b9bc8301fa9b0f95ad6ed22625be40da0fbf
Glsme <glasses.str.man@gmail.com>
2022-10-27 19:07:43 +0900

    Refactor: ImageListViewModel 코드 삭제

--- UnsplashMVVM/ImageList/ViewModel/ImageListViewModel.swift
+++ UnsplashMVVM/ImageList/ViewModel/ImageListViewModel.swift
@@ -9,13 +9,5 @@ import Foundation
 import RxSwift
 
 class ImageListViewModel {
-    var photoList = BehaviorSubject<SearchPhoto>(value: SearchPhoto(total: 0, totalPages: 0, results: []))
-    
-    func requestSearchPhoto(query: String) {
-        print(#function)
-        APIService.searchPhoto(query: query) { photo, status, error in
-            guard let photo = photo else { return }
-            self.photoList.onNext(photo)
-        }
-    }
+
 }
